86f742620b8d24c309ca5bc74a619ec4c17f9da4
Tyler Elliott <tylercal@gmail.com>
2021-09-03 09:14:17 -0700

    Update prefill.js
    use `let` so that variables are block scoped.

--- src/prefill.js
+++ src/prefill.js
@@ -1,9 +1,9 @@
 (function() {
     let login_field = document.querySelector('input[name="loginfmt"]');
-    var domain_hint = /domain_hint=([^&]+)/.exec(document.URL)[1];
+    let domain_hint = /domain_hint=([^&]+)/.exec(document.URL)[1];
     if (login_field && domain_hint){
         // Check domain hint is actually a domain
-        var is_domain = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.exec(domain_hint)
+        let is_domain = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.exec(domain_hint)
         if (is_domain) {
             login_field.value = "@" + domain_hint;
             login_field.setAttribute('type','text')
