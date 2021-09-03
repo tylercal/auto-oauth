a655406f4f3c1452256839c5f98f9b0edd04e943
Tim Hilliard <thilliard@linkedin.com>
2021-09-02 20:39:54 -0400

    set cursor position to beginning after adding domain hint

--- src/prefill.js
+++ src/prefill.js
@@ -6,6 +6,9 @@
         var is_domain = /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.exec(domain_hint)
         if (is_domain) {
             login_field.value = "@" + domain_hint;
+            login_field.setAttribute('type','text')
+            login_field.setSelectionRange(0,0)
+            login_field.setAttribute('type','email')
         }
     }
 })();
